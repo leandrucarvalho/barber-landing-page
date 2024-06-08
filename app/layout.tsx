535b3b57d90bc7c114be8f1f3c9d7857d9f8ff50
leandrucarvalho <leandrucs@gmail.com>
2024-06-07 21:15:30 -0300

    refector: update configs font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Barbearia",
   description: "Landing page de uma barbearia",
 };
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-  display: "swap",
-});
 
 export default function RootLayout({
   children,
