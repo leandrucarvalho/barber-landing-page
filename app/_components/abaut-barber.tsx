3ccd7e00b0fccf6dae7495a1ef7d70aa5910a182
leandrucarvalho <leandrucs@gmail.com>
2024-06-13 16:43:25 -0300

    style: updating the text

--- app/_components/abaut-barber.tsx
+++ app/_components/abaut-barber.tsx
@@ -4,7 +4,7 @@ const About = () => {
       <h2 className="text-4xl font-bold text-center">Sobre Mim</h2>
       <div className="flex flex-col items-center p-4">
         <p className="text-justify max-w-2xl">
-          sou profissional atuante na área de beleza, barbeiro e cabeleireiro,
+          Sou profissional atuante na área de beleza, barbeiro e cabeleireiro,
           onde sou especialista em degradê e cortes estilizados, venho com a
           proposta de não só trazer um ótimo serviço, mais entender o seu gosto
           e lhe proporcionar um momento ímpar onde a sua autoestima é
